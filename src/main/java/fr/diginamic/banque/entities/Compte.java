b8f31053603c456cff7e9ea93645c7ece70b8216
Tanguy Defer <tanguy59192@hotmail.fr>
2021-10-08 16:43:14 +0200

    Approche-objet-Refacto - Make numeroCompte final in Compte

--- src/main/java/fr/diginamic/banque/entities/Compte.java
+++ src/main/java/fr/diginamic/banque/entities/Compte.java
@@ -1,8 +1,8 @@
 package fr.diginamic.banque.entities;
 
 public class Compte {
-    private  String numeroCompte;
-    private  float soldeCompte;
+    private final String numeroCompte;
+    private float soldeCompte;
 
     public void setSoldeCompte(float soldeCompte) {
         this.soldeCompte = soldeCompte;
