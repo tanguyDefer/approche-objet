ac31a79f46569ae06c657cd70060bab249c26bbf
Tanguy Defer <tanguy59192@hotmail.fr>
2021-10-08 10:26:16 +0200

    éInitail commit

---
+++ src/main/java/fr/diginamic/approcheimperative/ApprocheImperativeApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.approcheimperative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprocheImperativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprocheImperativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/diginamic/approcheimperative/ApprocheImperativeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.approcheimperative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprocheImperativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
